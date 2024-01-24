34d2ba195ea9a26e759b22f58b1218fbf4b54186
ayushmajumdar14 <148596270+ayushmajumdar14@users.noreply.github.com>
2024-01-24 04:00:39 -0800

    Update views.py

--- example/views.py
+++ example/views.py
@@ -26,7 +26,7 @@ def index(request):
                 }}
                 .custom-text-container {{
                     max-width: 80%; /* Set the maximum width for the container */
-                    margin: 0 auto; /* Center the container horizontally */
+                    margin: 5vh auto; /* Center the container horizontally and adjust top margin */
                     background: linear-gradient(to right, #6A00FF, #8E24AA); /* Modify gradient colors as desired */
                     padding: 20px; /* Add padding for better visibility */
                     border-radius: 8px; /* Add border radius for rounded corners */
@@ -36,12 +36,8 @@ def index(request):
                     text-align: center; /* Center the text */
                 }}
                 .custom-text {{
-                    font-size: 40px; /* Adjust font size as needed */
+                    font-size: 24px; /* Adjust font size as needed */
                     color: white;
-                  
-                    max-width: 80%; /* Set the maximum width for the container */
-                    margin: 0 auto; /* Center the text horizontally */
-                    margin-top: 5vh; /* Adjust margin-top for positioning */
                 }}
                 .bold-text {{
                     font-weight: bold;
@@ -59,7 +55,8 @@ def index(request):
             <h1>ML/AI solution for Search Engine Optimization</h1>
             <p>WELCOME ANKITA, ANUNAY, ASISE, COREY, SARAYU, TYLER, AND AIMEE</p>
             <div class="custom-text-container">
-                <p class="custom-text">"We are a group of students dedicated to developing modern AI-powered solutions to enhance online presence."</p>
+                <p class="custom-text">"We are a group of UC Davis students aiming to develop modern AI-powered solutions to improve your website's online presence."</p>
+                <p class="bold-text">We are a group of students dedicated to developing modern AI-powered solutions to enhance online presence.</p>
             </div>
         </body>
     </html>
