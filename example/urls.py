fd723e488948f44bd01f7f4b0de2d585f94ee83d
ayushmajumdar14 <148596270+ayushmajumdar14@users.noreply.github.com>
2024-01-24 13:28:43 -0800

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
-# example/urls.py
 from django.urls import path
-
+from . import views
 from example.views import index
 
-
 urlpatterns = [
-    path('', index),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('project_leads/', views.project_leads, name='project_leads'),
+    path('developers/', views.developers, name='developers'),
+    path('journey/', views.journey, name='journey'),
 ]
