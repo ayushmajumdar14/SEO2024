3a32afd3f6d32e700fa64b7c0a8b52d6bdb25241
ayushmajumdar14 <148596270+ayushmajumdar14@users.noreply.github.com>
2024-01-24 14:53:46 -0800

    Create settings.py

---
+++ example/settings.py
@@ -0,0 +1,9 @@
+# settings.py
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # ...
+    },
+    # ...
+]
