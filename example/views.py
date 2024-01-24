4fcbf58b73ebe850ac49a34eead47165d1c17818
ayushmajumdar14 <148596270+ayushmajumdar14@users.noreply.github.com>
2024-01-24 02:13:17 -0800

    Update views.py

--- example/views.py
+++ example/views.py
@@ -7,9 +7,21 @@ def index(request):
     now = datetime.now()
     html = f'''
     <html>
+        <head>
+            <style>
+                body {{
+                    background-color: black;
+                    color: white;
+                    font-family: 'Roboto', sans-serif;
+                }}
+                h1, p {{
+                    text-align: center;
+                }}
+            </style>
+        </head>
         <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <h1>UC DAVIS DATA SCIENCE: SEO 2024 - AYUSH MAJUMDAR, AKSHAJ JOSHI</h1>
+            <p>WELCOME ANKITA, ANUNAY, ASISE, COREY, SARAYU, TYLER, AND AIMEE</p>
         </body>
     </html>
     '''
