1f977173acc1b05ab65970dbb903d6b9f59c8e2e
ayushmajumdar14 <148596270+ayushmajumdar14@users.noreply.github.com>
2024-01-24 03:42:47 -0800

    Update views.py

--- example/views.py
+++ example/views.py
@@ -22,13 +22,20 @@ def index(request):
                 }}
                 p {{
                     margin-top: 5vh;  /* 5% of the viewport height for subtitle */
+                    font-family: 'Roboto', sans-serif;
+                    color: white;
+                    background: linear-gradient(to right, #6A00FF, #8E24AA); /* Modify gradient colors as desired */
+                    padding: 10px; /* Add padding for better visibility */
+                    border-radius: 8px; /* Add border radius for rounded corners */
                 }}
             </style>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
         </head>
         <body>
             <h1>ML/AI solution for Search Engine Optimization</h1>
             <p>WELCOME ANKITA, ANUNAY, ASISE, COREY, SARAYU, TYLER, AND AIMEE</p>
+            <p>"We are a group of UC Davis students aiming to develop modern AI-powered solutions to improve your website's online presence."</p>
         </body>
     </html>
     '''
