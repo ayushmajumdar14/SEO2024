0a4ef7827f9cbbaf1ae9933d195227f2e246a97e
AkshajJ <76764226+AkshajJ@users.noreply.github.com>
2024-01-24 15:56:27 -0800

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from django.http import HttpResponse
+from django.shortcuts import render
 
+def about(request):
+    return render(request, 'about.html')
 def index(request):
     now = datetime.now()
     html = f'''
@@ -103,8 +106,7 @@ def index(request):
     return HttpResponse(html)
 
 
-def about(request):
-    return HttpResponse("<h2>About Us Page</h2>")
+
 
 
 def project_leads(request):
