39112be9f64e8c46e0e1ec2404827bfcd1dc8c9a
ayushmajumdar14 <148596270+ayushmajumdar14@users.noreply.github.com>
2024-01-24 03:05:27 -0800

    Update views.py

--- example/views.py
+++ example/views.py
@@ -21,13 +21,6 @@ def index(request):
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('project_leads/', views.project_leads, name='project_leads'),
-    path('developers/', views.developers, name='developers'),
-    path('journey/', views.journey, name='journey'),
-]
 <!-- templates/index.html -->
 <html>
 <head>
